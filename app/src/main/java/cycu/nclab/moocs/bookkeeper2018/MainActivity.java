606a193b492abdf05176491c8209682b44789475
hhliu <hunghuan.liu@gmail.com>
2018-09-07 02:19:43 +0800

    生命週期 (一)

--- app/src/main/java/cycu/nclab/moocs/bookkeeper2018/MainActivity.java
+++ app/src/main/java/cycu/nclab/moocs/bookkeeper2018/MainActivity.java
@@ -2,15 +2,53 @@ package cycu.nclab.moocs.bookkeeper2018;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    final String TAG = this.getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bookkeeping_relative_layout);
+        setContentView(R.layout.activity_bookkeeping_constraint_layout);
 
+        Log.i(TAG, TAG + ": enter onCreate()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, TAG + ": enter onStart()");
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i(TAG, TAG + ": enter onStop()");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i(TAG, TAG + ": enter onDestroy()");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i(TAG, TAG + ": enter onPause()");
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, TAG + ": enter onResume()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, TAG + ": enter onRestart()");
     }
 }
